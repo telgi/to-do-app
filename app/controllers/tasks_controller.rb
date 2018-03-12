bd598c71a638d0a77bad50ba5f2e232e9eba0c0d
Terry Allitt <tallitt88@gmail.com>
2018-03-12 14:01:10 +0000

    Add functionality for editing and updating a task object

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,16 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
   private
   # whitelisting parameters
   def task_params
