c99906836cdb9c23ed969a339b436871af7c2e55
Terry Allitt <tallitt88@gmail.com>
2018-03-12 22:22:01 +0000

    Change 'toggle completion' link to a checkbox that sits next to each task. Use conditional statement so that it only renders when you are updating a task, not creating one. Account for new redirect to go back via relative path when updating checkbox in show view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -39,9 +39,15 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def complete
+    @task = Task.find(params[:id])
+    @task.update_attribute(:completed, params[:completed])
+    redirect_back(fallback_location: root_path)
+  end
+
   private
   # whitelisting parameters
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
