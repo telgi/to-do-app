1b7665e01f8afc1f970b48ce0399c652c8a7cf0e
Terry Allitt <tallitt88@gmail.com>
2018-03-13 08:53:32 +0000

    Generate sessions controller, routes, and views - make tweaks to routes and views needed. Create template for new user session - log in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
-  resources :users
   get 'welcome/index'
   root 'welcome#index'
-  resources :tasks
 
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'log_in', to: 'sessions#new'
+  get 'log_out', to: 'sessions#destroy'
+
+  resources :users
+  get 'sign_up', to: 'users#new'
+
+  resources :tasks
   get 'tasks/:id/complete/:completed', to: 'tasks#complete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
