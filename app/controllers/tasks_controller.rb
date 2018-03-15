d0233f06775364f52ed9cce40c3c609c9b6121ef
Terry Allitt <tallitt88@gmail.com>
2018-03-15 14:51:26 +0000

    Use before_action callback methods to call confirm login, load_task, and confirm_owner as private methods before any TaskController actions are triggered. This fixes error where user must exist to create a task. Rails 'build' method in create method main piece of the jigsaw puzzle. Authorization functionality complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,13 @@
 class TasksController < ApplicationController
+
+  before_action :confirm_login
+  before_action :load_task, :confirm_owner, except: [:index, :new, :create]
+
   def index
     @tasks = current_user.tasks.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -12,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       redirect_to tasks_path
     else
@@ -21,11 +24,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_path
     else
@@ -34,13 +35,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
 
   def complete
-    @task = Task.find(params[:id])
     @task.update_attribute(:completed, params[:completed])
     redirect_back(fallback_location: root_path)
   end
@@ -49,5 +48,21 @@ class TasksController < ApplicationController
   # whitelisting parameters
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def load_task
+    @task = Task.find(params[:id])
+  end
+
+  def confirm_login
+    unless current_user
+      redirect_to root_path, alert: "Must log in to manage a To-Do List."
+    end
+  end
+
+  def confirm_owner
+    if @task && current_user != @task.user
+      redirect_to tasks_path, alert: "You do not have permission to access that task."
+    end
   end
 end
